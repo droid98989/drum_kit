6616ab9add366b9069167a4cffbddf529c2af578
droid98989 <97404390+droid98989@users.noreply.github.com>
2022-03-28 22:18:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   function handleClick() {
     var buttonClick = this.innerHTML;
     makeSound(buttonClick);
+    buttonAnimation(buttonClick)
   }
 }
 
@@ -13,6 +14,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.addEventListener("keydown", presser);
   function presser(event) {
     makeSound(event.key);
+    buttonAnimation(event.key)
   }
 
 
@@ -32,29 +34,3 @@ function makeSound(key) {
       var audio = new Audio('sounds/snare.mp3');
       audio.play();
       break;
-
-    case "d":
-      var audio = new Audio('sounds/tom-1.mp3');
-      audio.play();
-      break;
-
-    case "j":
-      var audio = new Audio('sounds/tom-2.mp3');
-      audio.play();
-      break;
-
-    case "k":
-      var audio = new Audio('sounds/tom-3.mp3');
-      audio.play();
-      break;
-
-    case "l":
-      var audio = new Audio('sounds/tom-4.mp3');
-      audio.play();
-      break;
-
-    default:
-      console.log(buttonClick);
-
-  }
-}
