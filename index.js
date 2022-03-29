9e119cc2e064a5f9ff15b40bc59cd13f798d2b8c
droid98989 <97404390+droid98989@users.noreply.github.com>
2022-03-28 22:21:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,37 @@ function makeSound(key) {
       var audio = new Audio('sounds/snare.mp3');
       audio.play();
       break;
+
+    case "d":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+
+    default:
+      console.log(buttonClick);
+
+  }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
